9cec8a944136cba5c69f19ef39df0c014c6ae873
Raymond Ehlers <raymond.ehlers@gmail.com>
2020-04-20 02:02:33 +0200

    Add response plots from skim

--- jet_substructure/analysis/plot_from_skim.py
+++ jet_substructure/analysis/plot_from_skim.py
@@ -41,7 +41,7 @@ class PlotConfig:
     log_y: bool = attr.ib(default=True)
 
 
-def _plot_residual(
+def _plot_residual_by_matching_type(
     hists: Dict[str, bh.Histogram],
     label: str,
     grooming_method: str,
@@ -155,11 +155,11 @@ def _plot_residual(
     plt.close(fig_simplified)
 
 
-def plot_residuals(
+def plot_residuals_by_matching_type(
     hists: Dict[str, bh.Histogram], grooming_methods: Sequence[str], matching_types: Sequence[str], output_dir: Path
 ) -> None:
     for grooming_method in grooming_methods:
-        _plot_residual(
+        _plot_residual_by_matching_type(
             hists=hists,
             label="jet_pt",
             grooming_method=grooming_method,
@@ -171,7 +171,7 @@ def plot_residuals(
             ),
             output_dir=output_dir,
         )
-        _plot_residual(
+        _plot_residual_by_matching_type(
             hists=hists,
             label="kt",
             grooming_method=grooming_method,
@@ -183,7 +183,7 @@ def plot_residuals(
             ),
             output_dir=output_dir,
         )
-        _plot_residual(
+        _plot_residual_by_matching_type(
             hists=hists,
             label="kt",
             grooming_method=grooming_method,
@@ -195,4 +195,316 @@ def plot_residuals(
             ),
             output_dir=output_dir,
             min_hybrid_kt=5,
+        )
+
+
+def _plot_residual_mean_and_width(
+    hists: Dict[str, bh.Histogram], grooming_method: str, plot_config: PlotConfig, output_dir: Path,
+) -> None:
+    logger.debug(f"Plotting jet pt residual mean and width for {grooming_method}")
+
+    fig_mean, ax_mean = plt.subplots(figsize=(8, 6))
+    fig_width, ax_width = plt.subplots(figsize=(8, 6))
+
+    for jet_types, label, color in [
+        ("hybrid_det", "Flucutations", "red"),
+        ("hybrid_true", "Combined", "black"),
+        ("det_true", "Detector", "blue"),
+    ]:
+        bh_hist = hists[f"{grooming_method}_{jet_types}_jet_pt_residual_mean"]
+        h = binned_data.BinnedData.from_existing_data(bh_hist)
+
+        # Plot.
+        ax_mean.errorbar(
+            h.axes[0].bin_centers,
+            h.values,
+            xerr=h.axes[0].bin_widths / 2,
+            marker=".",
+            linestyle="",
+            label=label,
+            alpha=0.8,
+            color=color,
+        )
+        # We want to plot the width (via the errors) of the width distribution
+        # We then normalize by the jet pt bin centers.
+        bh_hist_width = hists[f"{grooming_method}_{jet_types}_jet_pt_residual_width"]
+        h_width = binned_data.BinnedData.from_existing_data(bh_hist_width)
+        # Scale by the jet pt values
+        values = h_width.errors / h_width.axes[0].bin_centers
+        ax_width.errorbar(
+            h_width.axes[0].bin_centers,
+            values,
+            xerr=h_width.axes[0].bin_widths / 2,
+            marker=".",
+            linestyle="",
+            label=label,
+            alpha=0.8,
+            color=color,
+        )
+
+    # Labeling
+    # Individual labeling
+    ax_mean.set_ylabel(r"$(p_{\text{T}}^{\text{rec}} - p_{\text{T}}^{\text{part}}) / p_{\text{T}}^{\text{part}}$")
+    ax_mean.set_ylim([-1, 2])
+    ax_width.set_ylabel(
+        r"$\sigma((p_{\text{T}}^{\text{rec}} - p_{\text{T}}^{\text{part}})) / p_{\text{T}}^{\text{part}}$"
+    )
+    ax_width.set_ylim([0, 0.5])
+    # Shared
+    for a, f in [(ax_mean, fig_mean), (ax_width, fig_width)]:
+        text = "Iterative splittings"
+        text += "\n" + f"${helpers.RangeSelector(40, 120).display_str(label='hybrid')}$"
+        text += "\n" + " ".join(grooming_method.split("_")).capitalize()
+        a.text(
+            0.97,
+            0.97,
+            text,
+            transform=a.transAxes,
+            horizontalalignment="right",
+            verticalalignment="top",
+            multialignment="right",
+        )
+
+        # Presentation
+        a.set_xlabel(plot_config.x_label)
+        a.legend(frameon=False, loc="center right", fontsize=14)
+        f.tight_layout()
+    # Needs extra spacing on the left because the axis goes negative.
+    fig_mean.subplots_adjust(
+        # Reduce spacing between subplots
+        hspace=0,
+        wspace=0,
+        # Reduce external spacing
+        left=0.13,
+        bottom=0.12,
+        right=0.99,
+        top=0.98,
+    )
+    fig_width.subplots_adjust(
+        # Reduce spacing between subplots
+        hspace=0,
+        wspace=0,
+        # Reduce external spacing
+        left=0.10,
+        bottom=0.12,
+        right=0.99,
+        top=0.98,
+    )
+
+    # Store and cleanup
+    filename = f"{plot_config.name}_iterative_splittings_{grooming_method}"
+    fig_mean.savefig(output_dir / f"{filename}_mean.pdf")
+    plt.close(fig_mean)
+    fig_width.savefig(output_dir / f"{filename}_width.pdf")
+    plt.close(fig_width)
+
+
+def _plot_jet_pt_residual_with_pt_true_selection(
+    hists: Dict[str, bh.Histogram], grooming_method: str, plot_config: PlotConfig, output_dir: Path,
+) -> None:
+    """ Plot the full jet pt residual for a pt true selection.
+
+    Note:
+        The pt true selections was applied when filling. This just plots the values.
+    """
+    logger.debug(f"Plotting jet pt residual with pt true selection for {grooming_method}")
+
+    # Setup
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    # Colors are to match the jet substructure semi-central AN
+    for jet_types, label, color in [
+        ("hybrid_det", "Flucutations", "red"),
+        ("hybrid_true", "Combined", "black"),
+        ("det_true", "Detector", "blue"),
+    ]:
+        bh_hist = hists[f"{grooming_method}_{jet_types}_jet_pt_residual"]
+        h = binned_data.BinnedData.from_existing_data(bh_hist)
+
+        # Normalize
+        h /= np.sum(h.values)
+
+        ax.errorbar(
+            h.axes[0].bin_centers,
+            h.values,
+            yerr=h.errors,
+            xerr=h.axes[0].bin_widths / 2,
+            marker=".",
+            linestyle="",
+            label=label,
+            color=color,
+        )
+
+    # Labeling
+    text = "Iterative splittings"
+    text += "\n" + f"${helpers.RangeSelector(40, 120).display_str(label='hybrid')}$"
+    text += "\n" + f"${helpers.RangeSelector(40, 60).display_str(label='part')}$"
+    text += "\n" + " ".join(grooming_method.split("_")).capitalize()
+    ax.text(
+        0.97,
+        0.97,
+        text,
+        transform=ax.transAxes,
+        horizontalalignment="right",
+        verticalalignment="top",
+        multialignment="right",
+    )
+
+    # Presentation
+    ax.set_xlabel(plot_config.x_label)
+    ax.set_ylabel(plot_config.y_label)
+    ax.set_ylim([-0.02, 0.32])
+    ax.legend(frameon=False, loc="upper left", fontsize=14)
+    fig.tight_layout()
+    fig.subplots_adjust(
+        # Reduce spacing between subplots
+        hspace=0,
+        wspace=0,
+        # Reduce external spacing
+        left=0.10,
+        bottom=0.12,
+        right=0.99,
+        top=0.98,
+    )
+
+    # Store and cleanup
+    filename = f"{plot_config.name}_iterative_splittings_{grooming_method}"
+    fig.savefig(output_dir / f"{filename}.pdf")
+    plt.close(fig)
+
+
+def plot_residuals(hists: Dict[str, bh.Histogram], grooming_methods: Sequence[str], output_dir: Path) -> None:
+    for grooming_method in grooming_methods:
+        _plot_residual_mean_and_width(
+            hists=hists,
+            grooming_method=grooming_method,
+            plot_config=PlotConfig(
+                name="jet_pt_residual",
+                x_label=r"$p_{\text{T}}^{\text{part}}\:(\text{GeV}/c)$",
+                # Will be set individually during plotting of mean, width
+                y_label="IGNORE",
+            ),
+            output_dir=output_dir,
+        )
+
+        _plot_jet_pt_residual_with_pt_true_selection(
+            hists=hists,
+            grooming_method=grooming_method,
+            plot_config=PlotConfig(
+                name="jet_pt_residual_true_selection",
+                x_label=r"$(p_{\text{T}}^{\text{hybrid}} - p_{\text{T}}^{\text{det}}) / p_{\text{T}}^{\text{det}}$",
+                y_label="",
+            ),
+            output_dir=output_dir,
+        )
+
+
+def _plot_response_by_matching_type(
+    hists: Dict[str, bh.Histogram],
+    label: str,
+    grooming_method: str,
+    matching_types: Sequence[str],
+    plot_config: PlotConfig,
+    output_dir: Path,
+) -> None:
+
+    for matching_type in matching_types:
+        logger.debug(f"Plotting {label} response for {grooming_method}, {matching_type}")
+
+        matches_label = " ".join(matching_type.split("_")).capitalize()
+        bh_input_hist = hists[f"{grooming_method}_hybrid_det_{label}_response_matching_type_{matching_type}"]
+        h_input = binned_data.BinnedData.from_existing_data(bh_input_hist)
+
+        # Select the variables (for the example of kt)
+        # Axes: hybrid_pt, hybrid_kt, det_level_pt, det_level_kt
+        # NOTE: We already applied the 40 < hybrid jet pt < 120 cut, so it doesn't need an additional selection.
+        h = binned_data.BinnedData(
+            axes=[h_input.axes[1], h_input.axes[3]],
+            values=np.sum(h_input.values, axis=(0, 2)),
+            variances=np.sum(h_input.variances, axis=(0, 2)),
+        )
+
+        # Normalize the response.
+        normalization_values = h.values.sum(axis=0, keepdims=True)
+        h.values = np.divide(
+            h.values, normalization_values, out=np.zeros_like(h.values), where=normalization_values != 0
+        )
+
+        # Finish setup
+        fig, ax = plt.subplots(figsize=(8, 6))
+
+        # Determine the normalization range
+        z_axis_range = {
+            # "vmin": h_proj.values[h_proj.values > 0].min(),
+            "vmin": 1e-4,
+            "vmax": h.values.max(),
+        }
+
+        # Plot
+        mesh = ax.pcolormesh(
+            h.axes[0].bin_edges.T, h.axes[1].bin_edges.T, h.values.T, norm=matplotlib.colors.LogNorm(**z_axis_range),
+        )
+        fig.colorbar(mesh, pad=0.02)
+
+        # Labeling
+        text = "Iterative splittings"
+        text += "\n" + f"${helpers.RangeSelector(40, 120).display_str(label='hybrid')}$"
+        text += "\n" + " ".join(grooming_method.split("_")).capitalize()
+        text += "\n" + matches_label + " matches"
+        ax.text(
+            0.03,
+            0.97,
+            text,
+            transform=ax.transAxes,
+            horizontalalignment="left",
+            verticalalignment="top",
+            multialignment="left",
+        )
+
+        # Presentation
+        ax.set_xlabel(plot_config.x_label)
+        ax.set_ylabel(plot_config.y_label)
+        fig.tight_layout()
+        fig.subplots_adjust(
+            # Reduce spacing between subplots
+            hspace=0,
+            wspace=0,
+            # Reduce external spacing
+            left=0.10,
+            bottom=0.12,
+            right=0.99,
+            top=0.98,
+        )
+
+        # Store and cleanup
+        filename = f"{plot_config.name}_iterative_splittings_{grooming_method}_matching_type_{matching_type}"
+        fig.savefig(output_dir / f"{filename}.pdf")
+        plt.close(fig)
+
+
+def plot_response_by_matching_type(
+    hists: Dict[str, bh.Histogram], grooming_methods: Sequence[str], matching_types: Sequence[str], output_dir: Path
+) -> None:
+    for grooming_method in grooming_methods:
+        _plot_response_by_matching_type(
+            hists=hists,
+            label="kt",
+            grooming_method=grooming_method,
+            matching_types=matching_types,
+            plot_config=PlotConfig(
+                name="response_kt_hybrid_detector",
+                x_label=r"$k_{\text{T}}^{\text{hybrid}}\:(\text{{GeV}}/c)$",
+                y_label=r"$k_{\text{T}}^{\text{det}}\:(\text{{GeV}}/c)$",
+            ),
+            output_dir=output_dir,
+        )
+        _plot_response_by_matching_type(
+            hists=hists,
+            label="delta_R",
+            grooming_method=grooming_method,
+            matching_types=matching_types,
+            plot_config=PlotConfig(
+                name="response_delta_R_hybrid_detector", x_label=r"$R^{\text{hybrid}}$", y_label=r"$R^{\text{det}}$",
+            ),
+            output_dir=output_dir,
         )
