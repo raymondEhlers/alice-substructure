60682687ab23a7bdf258f844699ff15f11694976
Raymond Ehlers <raymond.ehlers@gmail.com>
2020-05-08 23:32:36 +0200

    Refactor unfolding plot

--- jet_substructure/analysis/plot_unfolding.py
+++ jet_substructure/analysis/plot_unfolding.py
@@ -3,9 +3,10 @@
 .. codeauthor:: Raymond Ehlers <raymond.ehlers@cern.ch>, ORNL
 """
 
+import functools
 import logging
 from pathlib import Path
-from typing import Mapping
+from typing import Callable, Mapping
 
 import boost_histogram as bh
 import matplotlib
@@ -23,50 +24,106 @@ logger = logging.getLogger(__name__)
 pachyderm.plot.configure()
 
 
-def plot_unfolded_kt(hists: Mapping[str, binned_data.BinnedData], output_dir: Path) -> None:
+def _efficiency_kt(
+    hists: Mapping[str, binned_data.BinnedData], true_jet_pt_bin: helpers.RangeSelector
+) -> binned_data.BinnedData:
+    """
+
+    """
+    # For convenience
+    bh_cut_efficiency = hists["true"].to_boost_histogram()
+    bh_full_efficiency = hists["truef"].to_boost_histogram()
+
+    # Select true pt range.
+    selection = slice(bh.loc(true_jet_pt_bin.min), bh.loc(true_jet_pt_bin.max), bh.sum)
+    cut = binned_data.BinnedData.from_existing_data(bh_cut_efficiency[:, selection])
+    full = binned_data.BinnedData.from_existing_data(bh_full_efficiency[:, selection])
+
+    return cut / full
+
+
+def _project_kt(input_hist: binned_data.BinnedData, true_jet_pt_bin: helpers.RangeSelector) -> binned_data.BinnedData:
+    # For convenience
+    bh_hist = input_hist.to_boost_histogram()
+
+    return binned_data.BinnedData.from_existing_data(
+        bh_hist[:, bh.loc(true_jet_pt_bin.min) : bh.loc(true_jet_pt_bin.max) : bh.sum]  # noqa: E203
+    )
+
+
+def _efficiency_pt(
+    hists: Mapping[str, binned_data.BinnedData], true_kt_bin: helpers.RangeSelector
+) -> binned_data.BinnedData:
+    """
+
+    """
+    # For convenience
+    bh_cut_efficiency = hists["true"].to_boost_histogram()
+    bh_full_efficiency = hists["truef"].to_boost_histogram()
+
+    # Select true pt range.
+    # selection = slice(bh.loc(true_kt_bin.min), bh.loc(true_kt_bin.max), bh.sum)
+    cut = binned_data.BinnedData.from_existing_data(bh_cut_efficiency[:: bh.sum, :])
+    full = binned_data.BinnedData.from_existing_data(bh_full_efficiency[:: bh.sum, :])
+
+    return cut / full
+
+
+def _project_pt(input_hist: binned_data.BinnedData, true_kt_bin: helpers.RangeSelector) -> binned_data.BinnedData:
+    """
+
+    """
+    bh_hist = input_hist.to_boost_histogram()
+
+    return binned_data.BinnedData.from_existing_data(bh_hist[:: bh.sum, :])
+
+
+def _normalize_unfolded(hist: binned_data.BinnedData, efficiency: binned_data.BinnedData) -> binned_data.BinnedData:
+    # Apply the efficiency.
+    hist /= efficiency
+    # Then normalize by the integral (sum) and bin width.
+    hist /= np.sum(hist.values)
+    hist /= hist.axes[0].bin_widths
+    return hist
+
+
+def plot_unfolded(
+    hists: Mapping[str, binned_data.BinnedData],
+    projection_func: Callable[[binned_data.BinnedData, helpers.RangeSelector], binned_data.BinnedData],
+    efficiency_func: Callable[[Mapping[str, binned_data.BinnedData], helpers.RangeSelector], binned_data.BinnedData],
+    n_iter_for_ratio: int,
+    label: str,
+    true_bin: helpers.RangeSelector,
+    output_dir: Path,
+) -> None:
     """ Plot unfolded.
 
     """
-    logger.debug("Plotting unfolded kt")
+    logger.debug(f"Plotting unfolded {label}")
     # Setup
-    # True jet pt range
-    true_jet_pt_range = helpers.RangeSelector(60, 80)
+    symbol_label = label[:1]
     fig, axes = plt.subplots(2, 1, figsize=(10, 10), gridspec_kw={"height_ratios": [3, 1]}, sharex=True,)
     ax_upper, ax_lower = axes
 
-    # We need the efficiency in the jet pt range that we actually want to measure.
-    efficiency = hists[f"correff{true_jet_pt_range.min:g}-{true_jet_pt_range.max:g}"]
+    # We need the efficiency in the true bin that we actually want to measure.
+    efficiency = efficiency_func(hists, true_bin)
+    # For convenience in normalizing.
+    _normalize_hist = functools.partial(_normalize_unfolded, efficiency=efficiency)
 
     # True
     # Project true onto the kt axis. We use boost histogram for the convince.
-    bh_true_2d = hists["true"].to_boost_histogram()
-    hist_true = binned_data.BinnedData.from_existing_data(bh_true_2d[:, bh.loc(60) : bh.loc(80) : bh.sum])
-    # Apply the efficiency.
-    hist_true /= efficiency
-    # Then normalize by the integral (sum) and bin width.
-    hist_true /= np.sum(hist_true.values)
-    hist_true /= hist_true.axes[0].bin_widths
+    hist_true = projection_func(hists["true"], true_bin)
+    # Normalize
+    hist_true = _normalize_hist(hist_true)
 
     # Determine ratio denominator.
-    n_iter = 6
-    # Retrieve the hist and normalize it properly.
-    bh_ratio_denominator_2d = hists[f"Bayesian_Unfoldediter{n_iter}"].to_boost_histogram()
-    # TODO: What is the right jet pt range here??
-    h_ratio_denominator = binned_data.BinnedData.from_existing_data(
-        bh_ratio_denominator_2d[:, bh.loc(60) : bh.loc(80) : bh.sum]
-    )
-    h_ratio_denominator /= efficiency
-    h_ratio_denominator /= np.sum(h_ratio_denominator.values)
-    h_ratio_denominator /= h_ratio_denominator.axes[0].bin_widths
+    h_ratio_denominator = projection_func(hists[f"Bayesian_Unfoldediter{n_iter_for_ratio}"], true_bin)
+    h_ratio_denominator = _normalize_hist(h_ratio_denominator)
 
     for i in range(1, 10):
         # Retrieve the hist and normalize it properly.
-        bh_2d = hists[f"Bayesian_Unfoldediter{i}"].to_boost_histogram()
-        # TODO: What is the right jet pt range here??
-        hist = binned_data.BinnedData.from_existing_data(bh_2d[:, bh.loc(60) : bh.loc(80) : bh.sum])
-        hist /= efficiency
-        hist /= np.sum(hist.values)
-        hist /= hist.axes[0].bin_widths
+        hist = projection_func(hists[f"Bayesian_Unfoldediter{i}"], true_bin)
+        hist = _normalize_hist(hist)
 
         ax_upper.errorbar(
             hist.axes[0].bin_centers,
@@ -111,11 +168,11 @@ def plot_unfolded_kt(hists: Mapping[str, binned_data.BinnedData], output_dir: Pa
     ax_upper.yaxis.set_major_locator(matplotlib.ticker.LogLocator(base=10.0, subs=(1.0,), numticks=100))
     ax_upper.yaxis.set_minor_locator(matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10) * 0.1, numticks=100))
     ax_upper.legend(loc="upper right", frameon=False)
-    ax_upper.set_ylabel(r"$\text{d}N/\text{d}k_{\text{T}}\:(\text{GeV}/c)^{-1}$")
+    ax_upper.set_ylabel(fr"$\text{{d}}N/\text{{d}}{symbol_label}_{{\text{{T}}}}\:(\text{{GeV}}/c)^{{-1}}$")
 
     # Finalize presentation for lower panel
     ax_lower.set_xlabel(r"$k_{\text{T}}\:(\text{GeV}/c$)")
-    ax_lower.set_ylabel(fr"Ratio to iter {n_iter}")
+    ax_lower.set_ylabel(fr"Ratio to iter {n_iter_for_ratio}")
     ax_lower.set_ylim([0, 2])
     fig.align_ylabels()
 
@@ -130,107 +187,7 @@ def plot_unfolded_kt(hists: Mapping[str, binned_data.BinnedData], output_dir: Pa
         top=0.98,
         bottom=0.07,
     )
-    fig.savefig(output_dir / "unfolded_kt.pdf")
-    plt.close(fig)
-
-
-def plot_unfolded_pt(hists: Mapping[str, binned_data.BinnedData], output_dir: Path) -> None:
-    """ Plot unfolded.
-
-    """
-    logger.debug("Plotting unfolded pt")
-    # Setup
-    fig, axes = plt.subplots(2, 1, figsize=(10, 10), gridspec_kw={"height_ratios": [3, 1]}, sharex=True,)
-    ax_upper, ax_lower = axes
-
-    # NOTE: We don't output the pt efficiency (?)
-    # TODO: Do we need to restrict the kt range here?
-
-    # True
-    # Project true onto the pt axis.
-    bh_true_2d = hists["true"].to_boost_histogram()
-    hist_true = binned_data.BinnedData.from_existing_data(bh_true_2d[:: bh.sum, :])
-    hist_true /= np.sum(hist_true.values)
-    hist_true /= hist_true.axes[0].bin_widths
-
-    # Determine ratio denominator.
-    n_iter = 4
-    # Retrieve the hist and normalize it properly.
-    bh_ratio_denominator_2d = hists[f"Bayesian_Unfoldediter{n_iter}"].to_boost_histogram()
-    h_ratio_denominator = binned_data.BinnedData.from_existing_data(bh_ratio_denominator_2d[:: bh.sum, :])
-    h_ratio_denominator /= np.sum(h_ratio_denominator.values)
-    h_ratio_denominator /= h_ratio_denominator.axes[0].bin_widths
-
-    for i in range(1, 10):
-        # Retrieve the hist and normalize it properly.
-        bh_2d = hists[f"Bayesian_Unfoldediter{i}"].to_boost_histogram()
-        hist = binned_data.BinnedData.from_existing_data(bh_2d[:: bh.sum, :])
-        hist /= np.sum(hist.values)
-        hist /= hist.axes[0].bin_widths
-
-        ax_upper.errorbar(
-            hist.axes[0].bin_centers,
-            hist.values,
-            xerr=hist.axes[0].bin_widths / 2,
-            yerr=hist.errors,
-            label=f"Bayes {i}",
-            marker="o",
-            linestyle="",
-            alpha=0.8,
-        )
-
-        ratio = hist / h_ratio_denominator
-        ax_lower.errorbar(
-            ratio.axes[0].bin_centers,
-            ratio.values,
-            xerr=ratio.axes[0].bin_widths / 2,
-            yerr=ratio.errors,
-            marker="o",
-            linestyle="",
-            alpha=0.8,
-        )
-
-    # Plot truth
-    # ax_upper.errorbar(hist_true.axes[0].bin_centers, hist_true.values, xerr=hist_true.axes[0].bin_widths / 2, yerr=hist_true.errors, label = "True",
-    #            marker="o", linestyle="", color="red")
-
-    # Draw reference line for ratio
-    ax_lower.axhline(y=1, color="black", linestyle="dashed", zorder=1)
-
-    # trueptd, which comes from the fully efficient truth and therefore doesn't need the kinematic efficiency correction
-    # hist_true = histogram.Histogram1D.from_existing_hist(hists["trueptd"])
-    ## Don't need to correct for the kinematic efficiency here.
-    # value, error = hist_true.counts_in_interval(min_bin=0, max_bin=len(hist_true.x))
-    # hist_true /= value
-    # hist_true /= hist_true.bin_widths
-    # ax.errorbar(hist_true.x, hist_true.y, xerr=hist_true.bin_widths / 2, yerr=hist_true.errors, label = "True (ptd)",
-    #            marker="o", linestyle="")
-
-    # Finalize presentation for upper panel
-    ax_upper.set_yscale("log")
-    ax_upper.yaxis.set_major_locator(matplotlib.ticker.LogLocator(base=10.0, subs=(1.0,), numticks=100))
-    ax_upper.yaxis.set_minor_locator(matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10) * 0.1, numticks=100))
-    ax_upper.legend(loc="upper right", frameon=False)
-    ax_upper.set_ylabel(r"$\text{d}N/\text{d}p_{\text{T}}\:(\text{GeV}/c)^{-1}$")
-
-    # Finalize presentation for lower panel
-    ax_lower.set_xlabel(r"$p_{\text{T}}\:(\text{GeV}/c$)")
-    ax_lower.set_ylabel(fr"Ratio to iter {n_iter}")
-    ax_lower.set_ylim([0, 2])
-    fig.align_ylabels()
-
-    fig.tight_layout()
-    fig.subplots_adjust(
-        # Reduce spacing between subplots
-        wspace=0,
-        hspace=0,
-        # Reduce external spacing
-        left=0.12,
-        right=0.98,
-        top=0.98,
-        bottom=0.07,
-    )
-    fig.savefig(output_dir / "unfolded_pt.pdf")
+    fig.savefig(output_dir / f"unfolded_{label}.pdf")
     plt.close(fig)
 
 
@@ -483,10 +440,28 @@ def run() -> None:
 
         # with sns.color_palette("GnBu_d", n_colors=11):
         with sns.color_palette("Paired", n_colors=11):
-            plot_unfolded_kt(hists, output_dir)
-            plot_unfolded_pt(hists, output_dir)
+            plot_unfolded(
+                hists=hists,
+                projection_func=_project_kt,
+                efficiency_func=_efficiency_kt,
+                n_iter_for_ratio=6,
+                label="kt",
+                true_bin=helpers.RangeSelector(60, 80),
+                output_dir=output_dir,
+            )
+            plot_unfolded(
+                hists=hists,
+                projection_func=_project_pt,
+                efficiency_func=_efficiency_pt,
+                n_iter_for_ratio=6,
+                label="pt",
+                true_bin=helpers.RangeSelector(0, 25),
+                output_dir=output_dir,
+            )
             plot_refolded_kt(hists, smeared_input, output_dir)
             plot_refolded_pt(hists, smeared_input, output_dir)
+
+        # TODO: Plot efficiency
         # plot_spectra_comparison(hists, output_dir)
         # plot_spectra_comparison_fine_binned(hists, output_dir)
         # plot_efficiency(hists, output_dir)
