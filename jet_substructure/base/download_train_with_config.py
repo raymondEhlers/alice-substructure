a06d54cf80076522ba757169fe1791763ed52d29
Raymond Ehlers <raymond.ehlers@gmail.com>
2020-05-19 14:03:14 +0200

    Add download scripts
    This still need a bit of cleanup and generalization, but they're in
    fairly good shape at the moment

---
+++ jet_substructure/base/download_train_with_config.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+""" Download files based on the config.yaml
+
+"""
+
+from pathlib import Path
+from typing import List, Tuple
+
+import ruamel.yaml
+import pachyderm.alice.utils as alice_utils
+import subprocess
+import xml.etree.ElementTree as ET
+
+
+_possible_merging_stages = ["merged", "Stage_1", "Stage_2", "Stage_5"]
+
+y = ruamel.yaml.YAML(typ="rt")
+
+#for run_number in range(5696, 5706):
+#    # It failed...
+#    if run_number == 5704:
+#        continue
+output = {}
+for train_number in range(5897, 5902):
+    print(f"Processing train {train_number}")
+    local_train_dir = Path(str(train_number))
+    config_filename = local_train_dir / "config.yaml"
+    with open(config_filename, "r") as f:
+        config = y.load(f)
+
+    # Sanity check
+    # If this fails, it means I probably forgot to update the run number in the YAML config.
+    assert int(config["number"]) == train_number
+
+    # Determine train properties.
+    base_alien_path = Path("/alice/cern.ch/user/a/alitrain/")
+    PWG = config.get("PWG", "PWGJE")
+    train_name = config.get("train")
+    base_alien_path = base_alien_path / PWG / train_name
+    train_directories_on_alien = alice_utils.list_alien_dir(base_alien_path)
+    possible_directories = [dir for dir in train_directories_on_alien if dir.startswith(str(train_number))]
+    # NOTE: There could be more than 1 directory if there are children. We only have to check for whether it's empty.
+    if len(possible_directories) == 0:
+        print(f"Can't find any directories for train. Skipping {train_number}.")
+        continue
+
+    alien_output_info = config["alien_output_info"]
+    for child_name, child_info in alien_output_info.items():
+        # Validation
+        child_name = child_name.lower()
+
+        # Determine the corresponding AliEn directory.
+        possible_child_directories = [dir for dir in possible_directories if dir.endswith(child_name)]
+        if len(possible_child_directories) != 1:
+            print(f"Could not find train directory corresponding to child {child_name}. Continuing")
+            continue
+
+        # Up until now, the possible directories have been relative to the base_alien_path.
+        # Since we're getting close to downloading or saving, we add it back it so we have an absolute path.
+        alien_dir = base_alien_path / Path(possible_child_directories[0]) / "merge"
+        # Extract values from the config needed to finally determine the files to download.
+        # Last successful merging stage, which we will use to determine what to download.
+        # We force the user to record the stage (rather than determine it automatically) so the record will be saved.
+        stage_to_download = child_info["stage_to_download"]
+        # Validation
+        if stage_to_download not in _possible_merging_stages:
+            raise ValueError(f"Invalid last successful merging stage. Provided: {stage_to_download}. Possible values: {_possible_merging_stages}")
+        # Child label (such as LHC18q)
+        child_label = child_info.get("name", child_name)
+        # Validation
+        child_label = child_label.replace("LHC", "").replace("lhc", "")
+
+        if stage_to_download == "merged":
+            # We just want the final merge files.
+            local_file = local_train_dir / f"AnalysisResults.{child_label}.root"
+            # So we just save it.
+            output[str(alien_dir / "AnalysisResults.root")] = str(local_file)
+        else:
+            alien_xml_file = alien_dir / f"{stage_to_download}.xml"
+            local_xml_file = local_train_dir / f"{stage_to_download}_{child_name}.xml"
+            # Download the XML file...
+            print(f"Downloading {stage_to_download}.xml file: alien://{alien_xml_file} to {local_xml_file}")
+            process = subprocess.run(
+                ["alien_cp", f"alien://{str(alien_xml_file)}", str(local_xml_file)],
+                stdout = subprocess.PIPE, stderr = subprocess.PIPE
+            )
+
+            # Open local XML file
+            tree = ET.parse(local_xml_file)
+            root = tree.getroot()
+            collection = root[0]
+            # Extract the filenames
+            for node in collection:
+                try:
+                    lfn = node[0].attrib["lfn"]
+                    alien_file = lfn.replace("root_archive.zip", "AnalysisResults.root")
+                    label = int(node.attrib["name"])
+                    local_file = local_train_dir / f"AnalysisResults.{child_label}.{label:02}.root"
+                    #print(f"Adding alien://{alien_file} : {local_file}")
+                    output[str(alien_file)] = str(local_file)
+                    #print(f"Downloading alien://{alien_file} to {local_file}")
+                    #process = subprocess.run(
+                    #    ["alien_cp", f"alien://{str(alien_file)}", str(local_file)],
+                    #    stdout = subprocess.PIPE, stderr = subprocess.PIPE
+                    #)
+                except IndexError:
+                    pass
+
+# Write out the files
+with open("files_to_download.yaml", "w") as f:
+    y.dump(output, f)
