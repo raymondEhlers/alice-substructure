20f61c2dbedeabda8a8d0bf387fe7d74a47bd98b
Raymond Ehlers <raymond.ehlers@gmail.com>
2020-05-27 00:25:44 +0200

    Various updates to the fall back

--- jet_substructure/cpp/embeddingFallBack.cc
+++ jet_substructure/cpp/embeddingFallBack.cc
@@ -2,8 +2,10 @@
 #include <string>
 #include <vector>
 
+#include <TFile.h>
 #include <TTree.h>
 #include <TTreeReader.h>
+#include <TChain.h>
 #include <TH1.h>
 #include <TH2F.h>
 
@@ -11,21 +13,47 @@
 void embeddingFallBack()
 {
   // Setup
-  std::string hybridPrefix = "hybrid";
-  std::string truePrefix = "true";
   std::string groomingMethod = "leading_kt";
+  std::string hybridPrefix = "hybrid";
+  std::string detLevelPrefix = "det_level";
+  std::string truePrefix = "true";
   std::string substructureVariableName = "kt";
   std::string outputFilename = "embeddingResponse_";
   outputFilename += substructureVariableName;
   outputFilename += "_grooming_method_";
   outputFilename += groomingMethod;
+  outputFilename += ".root";
 
   // Define hists
+  // Response hists
   std::vector<TH1*> hists;
-  TH2F hHybridTrueKtResponse("hHybridTrueKtResponse", "hHybridTrueKtResponse", 25, 0, 25, 25, 0, 25);
+  TH2F hHybridTrueKtResponse("hHybridTrueKtResponse", "hHybridTrueKtResponse", 26, -1, 25, 26, -1, 25);
   hists.push_back(&hHybridTrueKtResponse);
+  TH2F hHybridDetKtResponse("hHybridDetKtResponse", "hHybridDetKtResponse", 26, -1, 25, 26, -1, 25);
+  hists.push_back(&hHybridDetKtResponse);
+  TH2F hHybridTrueKtResponsePureMatches("hHybridTrueKtResponsePureMatches", "hHybridTrueKtResponsePureMatches", 26, -1, 25, 26, -1, 25);
+  hists.push_back(&hHybridTrueKtResponsePureMatches);
+  TH2F hHybridDetKtResponsePureMatches("hHybridDetKtResponsePureMatches", "hHybridDetKtResponsePureMatches", 26, -1, 25, 26, -1, 25);
+  hists.push_back(&hHybridDetKtResponsePureMatches);
+  // Matching hists
+  TH1D hHybridDetMatchingAll("hHybridDetMatchingAll", "hHybridDetMatchingAll", 150, 0, 150);
+  hists.push_back(&hHybridDetMatchingAll);
+  TH1D hHybridDetMatchingPure("hHybridDetMatchingPure", "hHybridDetMatchingPure", 150, 0, 150);
+  hists.push_back(&hHybridDetMatchingPure);
+  TH1D hHybridDetMatchingLeadingUntaggedSubleadingCorrect("hHybridDetMatchingLeadingUntaggedSubleadingCorrect", "hHybridDetMatchingLeadingUntaggedSubleadingCorrect", 150, 0, 150);
+  hists.push_back(&hHybridDetMatchingLeadingUntaggedSubleadingCorrect);
+  TH1D hHybridDetMatchingLeadingCorrectSubleadingUntagged("hHybridDetMatchingLeadingCorrectSubleadingUntagged", "hHybridDetMatchingLeadingCorrectSubleadingUntagged", 150, 0, 150);
+  hists.push_back(&hHybridDetMatchingLeadingCorrectSubleadingUntagged);
+  TH1D hHybridDetMatchingLeadingUntaggedSubleadingMistag("hHybridDetMatchingLeadingUntaggedSubleadingMistag", "hHybridDetMatchingLeadingUntaggedSubleadingMistag", 150, 0, 150);
+  hists.push_back(&hHybridDetMatchingLeadingUntaggedSubleadingMistag);
+  TH1D hHybridDetMatchingLeadingMistagSubleadingUntagged("hHybridDetMatchingLeadingMistagSubleadingUntagged", "hHybridDetMatchingLeadingMistagSubleadingUntagged", 150, 0, 150);
+  hists.push_back(&hHybridDetMatchingLeadingMistagSubleadingUntagged);
+  TH1D hHybridDetMatchingSwap("hHybridDetMatchingSwap", "hHybridDetMatchingSwap", 150, 0, 150);
+  hists.push_back(&hHybridDetMatchingSwap);
+  TH1D hHybridDetMatchingBothUntagged("hHybridDetMatchingBothUntagged", "hHybridDetMatchingBothUntagged", 150, 0, 150);
+  hists.push_back(&hHybridDetMatchingBothUntagged);
 
-  for (h : hists) {
+  for (auto h : hists) {
     h->Sumw2();
   }
 
@@ -52,29 +80,36 @@ void embeddingFallBack()
   embeddedChain.Add("/clusterfs4/rehlers/substructure/trains/embedPythia/5902/skim/merged/*.root");
   embeddedChain.Add("/clusterfs4/rehlers/substructure/trains/embedPythia/5903/skim/merged/*.root");
 
-  std::string truePrefix = "true";
-  std::string hybridPrefix = "hybrid";
   TTreeReader mcReader(&embeddedChain);
   TTreeReaderValue<float> scaleFactor(mcReader, "scale_factor");
   TTreeReaderValue<float> hybridJetPt(mcReader, ("jet_pt_" + hybridPrefix).c_str());
-  TTreeReaderValue<float> hybridSubstructureVariable(mcReader, (groomingMethod + "_" + hybridPrefix + "_" + substructureVariableName).c_str());
+  TTreeReaderValue<float> detLevelJetPt(mcReader, ("jet_pt_" + detLevelPrefix).c_str());
   TTreeReaderValue<float> trueJetPt(mcReader, ("jet_pt_" + truePrefix).c_str());
+  TTreeReaderValue<float> hybridSubstructureVariable(mcReader, (groomingMethod + "_" + hybridPrefix + "_" + substructureVariableName).c_str());
+  TTreeReaderValue<float> detLevelSubstructureVariable(mcReader, (groomingMethod + "_" + detLevelPrefix + "_" + substructureVariableName).c_str());
   TTreeReaderValue<float> trueSubstructureVariable(mcReader, (groomingMethod + "_" + truePrefix + "_" + substructureVariableName).c_str());
-  TTreeReaderValue<long long> matchingLeading(mcReader, (groomingMethod + "_hybrid_detector_matching_leading").c_str());
-  TTreeReaderValue<long long> matchingSubleading(mcReader, (groomingMethod + "_hybrid_detector_matching_subleading").c_str());
+  TTreeReaderValue<long long> matchingLeading(mcReader, (groomingMethod + "_hybrid_det_level_matching_leading").c_str());
+  TTreeReaderValue<long long> matchingSubleading(mcReader, (groomingMethod + "_hybrid_det_level_matching_subleading").c_str());
+  TTreeReaderValue<long long> matchingPartDetLeading(mcReader, (groomingMethod + "_det_level_true_matching_leading").c_str());
+  TTreeReaderValue<long long> matchingPartDetSubleading(mcReader, (groomingMethod + "_det_level_true_matching_subleading").c_str());
 
+  int counter = 0;
   while (mcReader.Next()) {
+    if (counter % 1000000 == 0) {
+        std::cout << "Jet: " << counter << "\n";
+    }
+    counter++;
     // Ensure that we are in the right true pt and substructure variable range.
-    if (*trueJetPt > 160) {
+    /*if (*trueJetPt > 160) {
       continue;
     }
     if (*trueSubstructureVariable > 100) {
       continue;
-    }
+    }*/
 
-    h2fulleff.Fill(*trueSubstructureVariable, *trueJetPt, *scaleFactor);
-    h2smearednocuts.Fill(*hybridSubstructureVariable, *hybridJetPt, *scaleFactor);
-    responsenotrunc.Fill(*hybridSubstructureVariable, *hybridJetPt, *trueSubstructureVariable, *trueJetPt, *scaleFactor);
+    //h2fulleff.Fill(*trueSubstructureVariable, *trueJetPt, *scaleFactor);
+    //h2smearednocuts.Fill(*hybridSubstructureVariable, *hybridJetPt, *scaleFactor);
+    //responsenotrunc.Fill(*hybridSubstructureVariable, *hybridJetPt, *trueSubstructureVariable, *trueJetPt, *scaleFactor);
 
     // Now start making cuts on the hybrid level.
     if (*hybridJetPt < 40 || *hybridJetPt > 120) {
@@ -96,16 +131,52 @@ void embeddingFallBack()
     if (usePureMatches && !(*matchingLeading == 1 && *matchingSubleading == 1)) {
       continue;
     }*/
-    hHybridTrueKtResponse.Fill(hybridSubstructureVariableValue, &trueSubstructureVariable);
+    hHybridTrueKtResponse.Fill(hybridSubstructureVariableValue, *trueSubstructureVariable, *scaleFactor);
+    hHybridDetKtResponse.Fill(hybridSubstructureVariableValue, *detLevelSubstructureVariable, *scaleFactor);
     //h2smeared.Fill(hybridSubstructureVariableValue, *hybridJetPt, *scaleFactor);
     //h2true.Fill(*trueSubstructureVariable, *trueJetPt, *scaleFactor);
     //response.Fill(hybridSubstructureVariableValue, *hybridJetPt, *trueSubstructureVariable, *trueJetPt, *scaleFactor);
+    // Pure matches
+    if (*matchingLeading == 1 && *matchingSubleading == 1) {
+        hHybridTrueKtResponsePureMatches.Fill(hybridSubstructureVariableValue, *trueSubstructureVariable, *scaleFactor);
+        hHybridDetKtResponsePureMatches.Fill(hybridSubstructureVariableValue, *detLevelSubstructureVariable, *scaleFactor);
+    }
 
-    // Next, if this works: matching
+    // Matching
+    hHybridDetMatchingAll.Fill(*detLevelJetPt);
+    if (*matchingLeading == 0) {
+        std::cout << "0\n";
+    }
+    if (*matchingLeading == 1 && *matchingSubleading == 1) {
+        hHybridDetMatchingPure.Fill(*detLevelJetPt);
+    }
+    if (*matchingLeading == 3 && *matchingSubleading == 1) {
+        hHybridDetMatchingLeadingUntaggedSubleadingCorrect.Fill(*detLevelJetPt);
+    }
+    if (*matchingLeading == 1 && *matchingSubleading == 3) {
+        hHybridDetMatchingLeadingCorrectSubleadingUntagged.Fill(*detLevelJetPt);
+    }
+    if (*matchingLeading == 3 && *matchingSubleading == 2) {
+        hHybridDetMatchingLeadingUntaggedSubleadingMistag.Fill(*detLevelJetPt);
+    }
+    if (*matchingLeading == 2 && *matchingSubleading == 3) {
+        hHybridDetMatchingLeadingMistagSubleadingUntagged.Fill(*detLevelJetPt);
+    }
+    if (*matchingLeading == 2 && *matchingSubleading == 2) {
+        hHybridDetMatchingSwap.Fill(*detLevelJetPt);
+    }
+    if (*matchingLeading == 3 && *matchingSubleading == 3) {
+        hHybridDetMatchingBothUntagged.Fill(*detLevelJetPt);
+    }
   }
 
   TFile fOut(outputFilename.c_str(), "RECREATE");
-  fout->cd();
-  hHybridTrueKtResponse->Write();
+  fOut.cd();
+  for (auto h: hists) {
+      h->Write();
+  }
 
+  // Cleanup
+  fOut.Write();
+  fOut.Close();
 }
