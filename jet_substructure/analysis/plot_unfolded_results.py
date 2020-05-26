15b1cc29ebc7e23dcfe232057784d1c11493380c
Raymond Ehlers <raymond.ehlers@gmail.com>
2020-05-26 21:10:58 +0200

    Add comparison data vs leading kt

--- jet_substructure/analysis/plot_unfolded_results.py
+++ jet_substructure/analysis/plot_unfolded_results.py
@@ -256,9 +256,166 @@ def plot_comparison_pythia(
     plt.close(fig)
 
 
-def plot_comparison(hists: Dict[str, Result], grooming_methods: Sequence[str], comparison_grooming_method: str) -> None:
-    ...
-    fig, ax = plt.subplots(figsize=(8, 6))
+def plot_comparison(
+    hists: Dict[str, Result],
+    grooming_methods: Sequence[str],
+    comparison_grooming_method: str,
+    plot_config: pb.PlotConfig,
+    output_dir: Path,
+) -> None:
+    # Setup
+    fig, (ax, ax_ratio) = plt.subplots(2, 1, figsize=(8, 6), gridspec_kw={"height_ratios": [3, 1]}, sharex=True,)
+    grooming_styles = pb.define_grooming_styles()
+
+    ratio_reference_hist = hists[comparison_grooming_method]
+
+    for grooming_method in grooming_methods:
+        # Setup
+        style = grooming_styles[grooming_method]
+        result = hists[grooming_method]
+
+        # Plot options
+        kwargs = {
+            "markerfacecolor": "white" if style.fillstyle == "none" else style.color,
+            "alpha": 1 if style.fillstyle == "none" else 0.8,
+        }
+        if style.fillstyle != "none":
+            kwargs["markeredgewidth"] = 0
+
+        ax.errorbar(
+            result.data.axes[0].bin_centers,
+            result.data.values,
+            yerr=result.data.errors,
+            xerr=result.data.axes[0].bin_widths / 2,
+            color=style.color,
+            marker=style.marker,
+            fillstyle=style.fillstyle,
+            linestyle="",
+            label=style.label,
+            zorder=style.zorder,
+            **kwargs,
+        )
+        # Systematic errors.
+        y_systematic_errors = result.data.metadata["y_systematic_errors"]
+        pachyderm.plot.error_boxes(
+            ax=ax,
+            x_data=result.data.axes[0].bin_centers,
+            y_data=result.data.values,
+            x_errors=result.data.axes[0].bin_widths / 2,
+            y_errors=np.array([y_systematic_errors.low, y_systematic_errors.high]),
+            color=style.color,
+            linewidth=0,
+            # label = "Background", color = plot_base.AnalysisColors.fit,
+        )
+
+        # Plot options
+        # kwargs = {
+        #    "markerfacecolor": "white" if pythia_style.fillstyle == "none" else pythia_style.color,
+        #    "alpha": 1 if pythia_style.fillstyle == "none" else 0.8,
+        # }
+        # if pythia_style.fillstyle != "none":
+        #    kwargs["markeredgewidth"] = 0
+
+        # ax.errorbar(
+        #    pythia.axes[0].bin_centers,
+        #    pythia.values,
+        #    # yerr=pythia.errors,
+        #    # xerr=pythia.axes[0].bin_widths / 2,
+        #    color=pythia_style.color,
+        #    # marker=pythia_style.marker,
+        #    fillstyle=pythia_style.fillstyle,
+        #    # linestyle="",
+        #    linewidth=3,
+        #    #label=f"Pythia, {pythia_style.label}",
+        #    label="Pythia 8 Monash 2013" if plotting_last_method else None,
+        #    zorder=pythia_style.zorder,
+        #    alpha=0.7,
+        # )
+        # ax.errorbar(
+        #    pythia.axes[0].bin_centers,
+        #    pythia.values,
+        #    # yerr=pythia.errors,
+        #    # xerr=pythia.axes[0].bin_widths / 2,
+        #    color=style.color,
+        #    # marker=style.marker,
+        #    fillstyle=style.fillstyle,
+        #    # linestyle="",
+        #    linewidth=3,
+        #    # label=f"Pythia, {pythia_style.label}",
+        #    label="Pythia 8 Monash 2013" if plotting_last_method else None,
+        #    zorder=pythia_style.zorder,
+        #    alpha=0.7,
+        # )
+
+        ratio = result.data / ratio_reference_hist.data
+        # Ratio + statistical error bars
+        ax_ratio.errorbar(
+            ratio.axes[0].bin_centers,
+            ratio.values,
+            yerr=ratio.errors,
+            xerr=ratio.axes[0].bin_widths / 2,
+            color=style.color,
+            marker=style.marker,
+            fillstyle=style.fillstyle,
+            linestyle="",
+            zorder=style.zorder,
+            **kwargs,
+        )
+        # Systematic errors.
+        y_relative_error_low = relative_error(
+            ErrorInput(value=result.data.values, error=result.data.metadata["y_systematic_errors"].low),
+            ErrorInput(
+                value=ratio_reference_hist.data.values,
+                error=ratio_reference_hist.data.metadata["y_systematic_errors"].low,
+            ),
+        )
+        y_relative_error_high = relative_error(
+            ErrorInput(value=result.data.values, error=result.data.metadata["y_systematic_errors"].high),
+            ErrorInput(
+                value=ratio_reference_hist.data.values,
+                error=ratio_reference_hist.data.metadata["y_systematic_errors"].high,
+            ),
+        )
+        # Sanity check
+        test_relative_y_error_low = np.sqrt(
+            (result.data.metadata["y_systematic_errors"].low / result.data.values) ** 2
+            + (ratio_reference_hist.data.metadata["y_systematic_errors"].low / ratio_reference_hist.data.values) ** 2
+        )
+        test_relative_y_error_high = np.sqrt(
+            (result.data.metadata["y_systematic_errors"].high / result.data.values) ** 2
+            + (ratio_reference_hist.data.metadata["y_systematic_errors"].high / ratio_reference_hist.data.values) ** 2
+        )
+        np.testing.assert_allclose(y_relative_error_low, test_relative_y_error_low)
+        np.testing.assert_allclose(y_relative_error_high, test_relative_y_error_high)
+        # From error prop, pythia has no systematic error, so we just convert the relative errors.
+        ratio.metadata["y_systematic_errors"] = AsymmetricErrors(
+            low=y_relative_error_low * ratio.values, high=y_relative_error_high * ratio.values,
+        )
+        y_systematic_errors = ratio.metadata["y_systematic_errors"]
+        pachyderm.plot.error_boxes(
+            ax=ax_ratio,
+            x_data=ratio.axes[0].bin_centers,
+            y_data=ratio.values,
+            x_errors=ratio.axes[0].bin_widths / 2,
+            y_errors=np.array([y_systematic_errors.low, y_systematic_errors.high]),
+            color=style.color,
+            linewidth=0,
+            # label = "Background", color = plot_base.AnalysisColors.fit,
+        )
+
+    # Reference value for ratio
+    ax_ratio.axhline(y=1, color="black", linestyle="dashed", zorder=1)
+
+    # Apply the PlotConfig
+    plot_config.apply(fig=fig, axes=[ax, ax_ratio])
+    # A few additional tweaks.
+    ax.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(base=1.0))
+    ax_ratio.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(base=0.2))
+
+    # filename = f"{plot_config.name}_{jet_pt_bin}{grooming_methods_filename_label}_{identifiers}_iterative_splittings"
+    filename = f"{plot_config.name}"
+    fig.savefig(output_dir / f"{filename}.pdf")
+    plt.close(fig)
 
 
 def run() -> None:
@@ -347,7 +504,36 @@ def run() -> None:
             ),
             output_dir=output_dir,
         )
-    plot_comparison(hists=hists, grooming_methods=list(hists.keys()), comparison_grooming_method="leading_kt")
+    plot_comparison(
+        hists=hists,
+        grooming_methods=list(hists.keys()),
+        comparison_grooming_method="leading_kt",
+        plot_config=pb.PlotConfig(
+            name=f"kt_comparison",
+            panels=[
+                # Main axis.
+                pb.Panel(
+                    axes=pb.AxisConfig(
+                        "y",
+                        label=r"$1/N_{\text{jets}}\:\text{d}N/\text{d}k_{\text{T}}\:(\text{GeV}/c)^{-1}$",
+                        log=True,
+                        range=(7e-3, 1),
+                    ),
+                    text=pb.TextConfig(x=0.96, y=0.96, text=text),
+                    legend=pb.LegendConfig(location="lower left", anchor=(0.02, 0.02)),
+                ),
+                # Ratio.
+                pb.Panel(
+                    axes=[
+                        pb.AxisConfig("x", label=r"$k_{\text{T}}\:(\text{GeV}/c)$", range=(-0.1, 8.6)),
+                        pb.AxisConfig("y", label=r"Data/Leading $k_{\text{T}}$", range=(0.5, 1.5)),
+                    ]
+                ),
+            ],
+            figure=pb.Figure(edge_padding={"left": 0.12, "top": 0.975, "right": 0.975}),
+        ),
+        output_dir=output_dir,
+    )
 
 
 if __name__ == "__main__":
