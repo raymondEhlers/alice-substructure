5f21ada39e83fe599115d73c37d6f2f754d6d0fa
Raymond Ehlers <raymond.ehlers@gmail.com>
2020-09-13 15:37:35 +0200

    Update conversion for nbconvert 6

--- notebooks/convert_remove_all_input.sh
+++ notebooks/convert_remove_all_input.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-jupyter nbconvert hardestKtSummary.ipynb \
+jupyter nbconvert hardestKtSummary.ipynb --to html \
     --TagRemovePreprocessor.remove_cell_tags='{"remove_cell"}' \
     --TagRemovePreprocessor.remove_all_outputs_tags='{"remove_output"}' \
     --TagRemovePreprocessor.remove_input_tags='{"remove_input"}' \
-     --no-input --no-prompt
+     --no-input --no-prompt --template "classic"
 
+# TODO: Convert to template "lab", but need to figure out how to keep it from filling the entire width of the page.
