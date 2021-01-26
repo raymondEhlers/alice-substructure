f114e74f9081fd72291b8926b46c1e421cc15575
Raymond Ehlers <raymond.ehlers@gmail.com>
2021-01-26 16:22:49 +0100

    Refactor ScaleFactor to base

--- jet_substructure/base/skim_analysis_objects.py
+++ jet_substructure/base/skim_analysis_objects.py
@@ -5,7 +5,50 @@
 
 from __future__ import annotations
 
+from typing import Any, Type
+
 import attr
+from pachyderm import binned_data
+
+
+@attr.s
+class ScaleFactor:
+    # float cast to ensure that we get a standard float instead of an np.float
+    cross_section: float = attr.ib(converter=float)
+    n_trials: float = attr.ib(converter=float)
+    n_entries: float = attr.ib(converter=float)
+
+    def value(self) -> float:
+        """Value of the scale factor.
+
+        NOTE:
+            Leticia's integral method (copied below) is the same as above if we didn't scale by n_entries.
+            However, I've historically scaled by n_entries, and will continue to do so here.
+            `scaleFactor = hcross->Integral(ptHardBin, ptHardBin) / htrials->Integral(ptHardBin, ptHardBin);`.
+
+        Args:
+            None.
+        Returns:
+            Scale factor calculated based on the extracted values.
+        """
+        return self.cross_section * self.n_entries / self.n_trials
+
+    @classmethod
+    def from_hists(cls: Type["ScaleFactor"], n_entries: int, cross_section: Any, n_trials: Any) -> "ScaleFactor":
+        # Validation
+        # (and for convenience)
+        h_cross_section = binned_data.BinnedData.from_existing_data(cross_section)
+        h_n_trials = binned_data.BinnedData.from_existing_data(n_trials)
+
+        # Find the first non-zero values bin.
+        # argmax will return the index of the first instance of True.
+        pt_hard_bin = (h_cross_section.values != 0).argmax(axis=0)
+
+        return cls(
+            cross_section=h_cross_section.values[pt_hard_bin],
+            n_trials=h_n_trials.values[pt_hard_bin],
+            n_entries=n_entries,
+        )
 
 
 @attr.s
--- jet_substructure/cpp/extract_scale_factors.py
+++ jet_substructure/cpp/extract_scale_factors.py
@@ -5,57 +5,16 @@
 
 import logging
 from pathlib import Path
-from typing import Any, Sequence, Tuple, Type
+from typing import Any, Sequence, Tuple
 
-import attr
 import numpy as np
 import uproot
 from pachyderm import binned_data, yaml
 
-from jet_substructure.base import helpers
+from jet_substructure.base import helpers, skim_analysis_objects
 
 
 logger = logging.getLogger(__name__)
-
-
-@attr.s
-class ScaleFactor:
-    # float cast to ensure that we get a standard float instead of an np.float
-    cross_section: float = attr.ib(converter=float)
-    n_trials: float = attr.ib(converter=float)
-    n_entries: float = attr.ib(converter=float)
-
-    def value(self) -> float:
-        """Value of the scale factor.
-
-        NOTE:
-            Leticia's integral method (copied below) is the same as above if we didn't scale by n_entries.
-            However, I've historically scaled by n_entries, and will continue to do so here.
-            `scaleFactor = hcross->Integral(ptHardBin, ptHardBin) / htrials->Integral(ptHardBin, ptHardBin);`.
-
-        Args:
-            None.
-        Returns:
-            Scale factor calculated based on the extracted values.
-        """
-        return self.cross_section * self.n_entries / self.n_trials
-
-    @classmethod
-    def from_hists(cls: Type["ScaleFactor"], n_entries: int, cross_section: Any, n_trials: Any) -> "ScaleFactor":
-        # Validation
-        # (and for convenience)
-        h_cross_section = binned_data.BinnedData.from_existing_data(cross_section)
-        h_n_trials = binned_data.BinnedData.from_existing_data(n_trials)
-
-        # Find the first non-zero values bin.
-        # argmax will return the index of the first instance of True.
-        pt_hard_bin = (h_cross_section.values != 0).argmax(axis=0)
-
-        return cls(
-            cross_section=h_cross_section.values[pt_hard_bin],
-            n_trials=h_n_trials.values[pt_hard_bin],
-            n_entries=n_entries,
-        )
 
 
 def scale_factor_ROOT_wrapper(base_path: Path, train_number: int) -> Tuple[int, Any, Any]:
@@ -130,7 +89,7 @@ def scale_factor_uproot(filenames: Sequence[Path], run_despite_issues: bool = Fa
 
 
 def scale_factor_from_hists(n_entries: int, cross_section: Any, n_trials: Any) -> float:
-    scale_factor = ScaleFactor.from_hists(
+    scale_factor = skim_analysis_objects.ScaleFactor.from_hists(
         cross_section=cross_section,
         n_trials=n_trials,
         n_entries=n_entries,
@@ -155,7 +114,7 @@ def test() -> None:
         # res_ROOT = scale_factor_ROOT(base_path=base_path, train_number=train_number)
         # res_uproot = scale_factor_uproot(base_path=base_path, train_number=train_number)
 
-    y = yaml.yaml(classes_to_register=[ScaleFactor])
+    y = yaml.yaml(classes_to_register=[skim_analysis_objects.ScaleFactor])
     with open("test.yaml", "w") as f:
         y.dump(scale_factors_ROOT, f)
 
