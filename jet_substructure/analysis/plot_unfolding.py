12acd12775b22b803d6df1c8bf65e70c3fe86a84
Raymond Ehlers <raymond.ehlers@gmail.com>
2022-04-05 15:12:00 +0200

    Load hybrid model predictions

--- jet_substructure/analysis/plot_unfolding.py
+++ jet_substructure/analysis/plot_unfolding.py
@@ -6,7 +6,7 @@
 import itertools
 import logging
 from pathlib import Path
-from typing import Callable, Dict, Iterable, Mapping, MutableMapping, Optional, Sequence, Tuple, Union
+from typing import Any, Callable, Dict, Iterable, Mapping, MutableMapping, Optional, Sequence, Tuple, Union
 
 import attr
 import boost_histogram as bh
@@ -14,6 +14,7 @@ import cycler
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 import pachyderm.plot
 import seaborn as sns
 import uproot
@@ -560,6 +561,167 @@ def load_sherpa_predictions(
     return output
 
 
+@attr.define
+class ModelInfo:
+    # TODO: Impelemtn this to wrap model predictions...
+    label: str
+    needs_normalization: bool = attr.field(default=False)
+    metadata: Dict[str, Any] = attr.field(factory=dict)
+
+
+def _load_hybrid_model(
+    base_dir: Path,
+    dyg_filename: str,
+    sd_filename: str,
+    bin_edges: Dict[str, Dict[str, npt.NDArray[np.float64]]],
+    jet_R: str,
+    jet_pt_bin: helpers.JetPtRange,
+    quantity_to_retrieve: str = "ratio",
+) -> Dict[str, Dict[str, binned_data.BinnedData]]:
+    output = {}
+
+    # Encode the file specification below
+    # First, the options in the filename
+    _wake_label = {
+        True: "WantWake",
+        # NOTE: I don't know it would look like without the wake. But since I don't have that file at
+        #       the moment, it's not really an issue.
+    }
+    #_moliere_label = {
+    #    True: "WithElastic",
+    #    False: "NoElastic",
+    #}
+    # Next, the encoding of the data
+    _kt_bin_center_index = 0
+    # First value is the upper bound, second is the lower bound.
+    _n_quantities = 6
+    _quantity_to_relative_index = {
+        "PbPb": (0, 1),
+        "pp": (2, 3),
+        "ratio": (4, 5),
+    }
+    _soft_drop_prediction_indices = {
+        "soft_drop_z_cut_00": 0,
+        "soft_drop_z_cut_02": 1,
+    }
+    _dyg_prediction_indices = {
+        "dynamical_core": 0,
+        "dynamical_kt": 1,
+        "dynamical_time": 2,
+    }
+    _pt_bin_index = {
+        helpers.JetPtRange(40, 60): 0,
+        helpers.JetPtRange(60, 80): 1,
+        helpers.JetPtRange(80, 100): 2,
+        helpers.JetPtRange(100, 150): 3,
+    }
+
+    # Here's what we should grab, manually calculated:
+    """
+    SD zcut=0     1:18:19 w filledcu. (From file ending with SD.dat)
+    SD zcut=0.2   1:24:25 w filledcu. (From file ending with SD.dat)
+    DyG a=0.5     1:24:25 w filledcu. (From file ending with DyG.dat)
+    DyG a=1       1:30:31 w filledcu. (From file ending with DyG.dat)
+    DyG a=2       1:36:37 w filledcu. (From file ending with DyG.dat)
+    """
+
+    output[jet_R] = {}
+    for grooming_method in [
+            "dynamical_core",
+            "dynamical_kt",
+            "dynamical_time",
+            "soft_drop_z_cut_02",
+        ]:
+
+        # This isn't especially efficient, but good enough for now
+        filename = base_dir
+        if "soft_drop" in grooming_method:
+            filename = base_dir / sd_filename
+        else:
+            filename = base_dir / dyg_filename
+        data = np.loadtxt(filename)
+
+        _axis = binned_data.Axis(bin_edges=bin_edges[jet_R][grooming_method])
+        _bin_centers = _axis.bin_centers
+        _values = []
+        _variances = []
+        _prediction_indices = _dyg_prediction_indices if "dynamical" in grooming_method else _soft_drop_prediction_indices
+        for i_kt, row in enumerate(data):
+            _bin_center = row[_kt_bin_center_index]
+            # Assuming these all match up, we can just use the provided bin centers
+            if _bin_center != _bin_centers[i_kt]:
+                raise ValueError(
+                    f"Mismatch betweeng file bin center {_bin_center} and provided bin center: {_bin_centers[i_kt]}"
+                )
+
+            _pt_bin_offset = _pt_bin_index[jet_pt_bin] * 6
+            _offset_for_bin_center = 1
+            _offset_to_desired_quantities = (
+                _offset_for_bin_center
+                + (_pt_bin_index[jet_pt_bin] * len(_prediction_indices) * _n_quantities)
+                + (_prediction_indices[grooming_method] * _n_quantities)
+            )
+            _relative_indices = _quantity_to_relative_index[quantity_to_retrieve]
+            # (lower, upper)
+            # TODO: Need to update plotting to plot with fill_between
+            band_indices = (
+                _offset_to_desired_quantities + _relative_indices[1],
+                _offset_to_desired_quantities + _relative_indices[0]
+            )
+            logger.info(
+                f"grooming_method: {grooming_method}: (lower_band_index, upper_band_index): {band_indices}"
+            )
+            band = (row[band_indices[0]], row[band_indices[1]])
+            central_value = sum(band)/len(band)
+            # Make it symmetric by construction.
+            error_squared = (central_value - band[0]) ** 2
+
+            _values.append(central_value)
+            _variances.append(error_squared)
+
+        output[jet_R][grooming_method] = binned_data.BinnedData(
+            axes=_axis,
+            values=_values,
+            variances=_variances,
+        )
+
+    return output
+
+
+def load_hybrid_model(
+    base_dir: Path,
+    bin_edges: Dict[str, Dict[str, npt.NDArray[np.float64]]],
+    jet_R: str,
+    jet_pt_bin: helpers.JetPtRange,
+    quantity_to_retrieve: str = "ratio",
+) -> Dict[str, Dict[str, Dict[str, binned_data.BinnedData]]]:
+    _moliere_label = {
+        True: "WithElastic",
+        False: "NoElastic",
+    }
+    _moliere_output_label = {
+        True: "hybrid_moliere",
+        False: "hybrid_without_moliere",
+    }
+
+    output = {}
+    for include_moliere in [False, True]:
+        dyg_filename_template: str = "3050_kT_{moliere_label}_WantWake_1_JetR_2_kT_DyG.dat"
+        sd_filename_template: str = "3050_kT_{moliere_label}_WantWake_1_JetR_2_kT_SD.dat"
+
+        output[_moliere_output_label[include_moliere]] = _load_hybrid_model(
+            base_dir=base_dir,
+            dyg_filename=dyg_filename_template.format(moliere_label=_moliere_label[include_moliere]),
+            sd_filename=sd_filename_template.format(moliere_label=_moliere_label[include_moliere]),
+            bin_edges=bin_edges,
+            jet_R=jet_R,
+            jet_pt_bin=jet_pt_bin,
+            quantity_to_retrieve=quantity_to_retrieve,
+        )
+
+    return output
+
+
 #_model_palette = sns.color_palette("husl", n_colors=6)
 #_model_palette = sns.color_palette("Accent", n_colors=10)
 #_model_palette = sns.color_palette("colorblind", n_colors=10)
@@ -640,6 +802,26 @@ _models_styles = {
         #color=_model_palette[8],
         #color=_model_palette[4],
         color=_model_palette[3],
+    ),
+    "hybrid_moliere": dict(
+        label="Hybrid model (Pablos et al) w/ Moliere",
+        linewidth=3,
+        linestyle="--",
+        marker="D",
+        #color=_model_palette[4],
+        #color=_model_palette[8],
+        #color=_model_palette[4],
+        color=_model_palette[5],
+    ),
+    "hybrid_without_moliere": dict(
+        label="Hybrid model (Pablos et al) w/o Moliere",
+        linewidth=3,
+        linestyle="--",
+        marker="D",
+        #color=_model_palette[4],
+        #color=_model_palette[8],
+        #color=_model_palette[4],
+        color=_model_palette[6],
     ),
 }
 
