b44d5402da818edd14c34b85a4ee08535b7cb4f8
Raymond Ehlers <raymond.ehlers@gmail.com>
2023-03-06 14:04:40 -0800

    Missing files and cleanup

--- notebooks/convert_remove_all_input.sh
+++ notebooks/convert_remove_all_input.sh
@@ -11,7 +11,6 @@ notebook=${1}
 #jupyter nbconvert hardestKtSummary.ipynb --to html --template ../.venv/lib/python3.8/site-packages/jupyter_contrib_nbextensions/templates/toc2.tpl \
 #jupyter nbconvert hardestKtSummary.ipynb --to html --template "html_with_toc" \
 #jupyter nbconvert hardestKtSummary.ipynb --to html --template "html_toc" \
-#jupyter nbconvert hardestKtSummary.ipynb --to html --template "test_html" \
 #jupyter nbconvert hardestKtSummary.ipynb --to html --template "classic" \
 #jupyter nbconvert hardestKtSummary.ipynb --to html --template "my_html_with_toc" \
 jupyter nbconvert ${notebook} --to html_embed --template "my_html_with_toc" \
