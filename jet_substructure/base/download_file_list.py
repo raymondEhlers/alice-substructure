4afcb9847ea94c53ccc9702007a8b7b4b48bf5ac
Raymond Ehlers <raymond.ehlers@gmail.com>
2023-06-22 11:52:50 -0700

    Port ALICE download scripts to mammoth

--- jet_substructure/base/download_file_list.py
+++ jet_substructure/base/download_file_list.py
@@ -1,48 +1,13 @@
-#!/usr/bin/env python3
+""" Download LEGO train output file pair list stored in YAML
 
-""" Download file list in YAML with pachyderm
+This was ported to mammoth, so we made this file into a stub
+which just imports the ported functionality.
 
-.. codeauthor:: Raymond Ehlers <raymond.ehlers@cern.ch>, ORNL
+.. codeauthor:: Raymond Ehlers <raymond.ehlers@cern.ch>, LBL/UCB
 """
 
-import logging
-import queue
-from pathlib import Path
-from typing import List
-
-from pachyderm import yaml
-from pachyderm.alice import download
-
-from jet_substructure.base import helpers
-
-logger = logging.getLogger(__name__)
-
-
-def create_file_pairs() -> List[download.FilePair]:
-    """Create file pairs.
-
-    YAML file is of the form:
-    alien_file: local_file
-    """
-    y = yaml.yaml()
-    with Path("files_to_download.yaml").open() as f:
-        file_list_input = y.load(f)
-
-    file_pairs = []
-    for alien_file, local_file in file_list_input.items():
-        file_pairs.append(download.FilePair(source=alien_file, target=local_file))
-
-    logger.debug(f"file_pairs: {file_pairs}")
-
-    return file_pairs
-
+from mammoth.alice import download_file_list
 
 if __name__ == "__main__":
-    helpers.setup_logging(level=logging.INFO)
+    download_file_list.run()
 
-    file_pairs = create_file_pairs()
-
-    # Setup the queue and filler, and then start downloading.
-    q: download.FilePairQueue = queue.Queue()
-    queue_filler = download.FileListDownloadFiller(pairs=file_pairs, q=q)
-    download.download(queue_filler=queue_filler, q=q)
