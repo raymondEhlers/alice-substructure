ffedba20a39463e76f0075093be1d05dbbb08100
Raymond Ehlers <raymond.ehlers@gmail.com>
2023-07-31 13:00:02 -0700

    Stats plots for all systems

--- notebooks/hardest-rg-zg-summary.py
+++ notebooks/hardest-rg-zg-summary.py
@@ -30,6 +30,7 @@ from mammoth import helpers as mammoth_helpers
 from jet_substructure.analysis import (
     model_calculations,
     plot_paper,
+    plot_from_skim,
     plot_unfolding,
     unfolding_analysis,
     plot_style
@@ -75,6 +76,109 @@ def grooming_name(grooming_method: str, prefixes: list[str]) -> str:
 # Settings
 collision_system = "pp"
 prefix = "data"
+#jet_pt_bin = helpers.JetPtRange(min=40, max=120)
+jet_pt_bin = helpers.JetPtRange(min=20, max=85)
+text_label = "Iterative splittings"
+text_label += "\n" + f"${jet_pt_bin.display_str(label='meas.')}$"
+tag = jet_pt_bin.histogram_str(prefix)
+system_label = plot_style.label_to_display_string["collision_system"][f"{collision_system}_5TeV"]
+
+# Setup
+#_all_available_methods = list(nb_utils.all_grooming_methods)
+_grooming_methods = [
+    "dynamical_core",
+    "dynamical_kt",
+    "dynamical_time",
+]
+_method_groups = [
+    _grooming_methods,
+    # Older
+    ##nb_utils.leading_kt_grooming_methods,
+    #nb_utils.dynamical_grooming_methods,
+    #nb_utils.soft_drop_grooming_methods,
+    #nb_utils.dynamical_grooming_with_z_cut,
+    #nb_utils.z_cut_02_grooming_methods,
+    ##_all_available_methods,
+]
+# We want to skip the leading_kt methods, but not redefine all_grooming_methods in nb_utils,
+# so we combine all of the above
+_all_available_methods = [_method for _group in _method_groups for _method in _group]
+# Add the add the all methods to the method groups
+_method_groups.append(_all_available_methods)
+
+# Output
+#_output_dir = output_dir / collision_system / "RDF" / "jupyter" / "2023-paper" /"semi_central_R02" / "pass3" / prefix
+_output_dir = output_dir / collision_system / "RDF" / "jupyter" / "2023-paper" /"pp_R02" / "pass1" / prefix
+_output_dir.mkdir(parents=True, exist_ok=True)
+_fig_output_dir = _output_dir / "png"
+
+# Load data for comparison
+hists_data_pp = {}
+_successfully_loaded_methods = []
+#dataset_name = "LHC18qr_semi_central_R02_6765"
+#dataset_name = "LHC18qr_semi_central_R02_0067"
+dataset_name = "LHC17pq_pp_R02_0060"
+#for grooming_method in nb_utils.all_grooming_methods:
+for grooming_method in _grooming_methods:
+    try:
+        hists_data_pp.update(nb_utils.load_histograms(
+            filename = f"{dataset_name}_{grooming_name(grooming_method, [prefix])}.root", collision_system=collision_system,
+            tag = "RDF", base_path = Path("output")
+        ))
+        _successfully_loaded_methods.append(grooming_method)
+    except FileNotFoundError:
+        logger.info(f"Skipping grooming method {grooming_method} because the output file isn't available")
+        
+# Convert to boost_histogram because it simplifies projections later.
+hists_data_pp = {k: v.to_boost_histogram() for k, v in hists_data_pp.items()}
+# -
+
+logger.info(f"\nSuccessfully loaded grooming methods: {_successfully_loaded_methods}")
+
+from importlib import reload
+reload(plot_from_skim)
+
+# ### Stats
+
+# Setup
+plot_png = False
+for smeared_min_kt in [None, 1.0, 1.5]:
+    _filenames = plot_from_skim.plot_Rg_vs_jet_pt_stats(
+        hists=hists_data_pp,
+        grooming_methods=_all_available_methods,
+        prefix=prefix,
+        jet_pt_bin=jet_pt_bin,
+        rdf_plots=True,
+        output_dir=_output_dir,
+        plot_png=plot_png,
+        system_label=system_label,
+        smeared_min_kt=smeared_min_kt,
+    )
+
+    _filenames = plot_from_skim.plot_zg_vs_jet_pt_stats(
+        hists=hists_data_pp,
+        grooming_methods=_all_available_methods,
+        prefix=prefix,
+        jet_pt_bin=jet_pt_bin,
+        rdf_plots=True,
+        output_dir=_output_dir,
+        plot_png=plot_png,
+        system_label=system_label,
+        smeared_min_kt=smeared_min_kt,
+    )
+
+
+# ## Central PbPb
+
+# ### Load data
+
+# +
+def grooming_name(grooming_method: str, prefixes: list[str]) -> str:
+    return f"{grooming_method}_prefixes_{'_'.join(prefixes)}"
+
+# Settings
+collision_system = "PbPb"
+prefix = "data"
 jet_pt_bin = helpers.JetPtRange(min=40, max=120)
 text_label = "Iterative splittings"
 text_label += "\n" + f"${jet_pt_bin.display_str(label='meas.')}$"
@@ -83,18 +187,125 @@ system_label = plot_style.label_to_display_string["collision_system"][f"{collisi
 
 # Setup
 #_all_available_methods = list(nb_utils.all_grooming_methods)
-_method_groups = [
-    #nb_utils.leading_kt_grooming_methods,
-    nb_utils.dynamical_grooming_methods,
-    nb_utils.soft_drop_grooming_methods,
-    nb_utils.dynamical_grooming_with_z_cut,
-    nb_utils.z_cut_02_grooming_methods,
-    #_all_available_methods,
-]
 _grooming_methods = [
     "dynamical_core",
     "dynamical_kt",
     "dynamical_time",
+]
+_method_groups = [
+    _grooming_methods,
+    # Older
+    ##nb_utils.leading_kt_grooming_methods,
+    #nb_utils.dynamical_grooming_methods,
+    #nb_utils.soft_drop_grooming_methods,
+    #nb_utils.dynamical_grooming_with_z_cut,
+    #nb_utils.z_cut_02_grooming_methods,
+    ##_all_available_methods,
+]
+# We want to skip the leading_kt methods, but not redefine all_grooming_methods in nb_utils,
+# so we combine all of the above
+_all_available_methods = [_method for _group in _method_groups for _method in _group]
+# Add the add the all methods to the method groups
+_method_groups.append(_all_available_methods)
+
+# Output
+_output_dir = output_dir / collision_system / "RDF" / "jupyter" / "2023-paper" /"central_R02" / "pass3" / prefix
+_output_dir.mkdir(parents=True, exist_ok=True)
+_fig_output_dir = _output_dir / "png"
+
+# Load data for comparison
+hists_data_central = {}
+_successfully_loaded_methods = []
+#dataset_name = "LHC18qr_semi_central_R02_6765"
+#dataset_name = "LHC18qr_semi_central_R02_0067"
+dataset_name = "LHC18qr_central_R02_0070"
+#for grooming_method in nb_utils.all_grooming_methods:
+for grooming_method in _grooming_methods:
+    try:
+        hists_data_central.update(nb_utils.load_histograms(
+            filename = f"{dataset_name}_{grooming_name(grooming_method, [prefix])}.root", collision_system=collision_system,
+            tag = "RDF", base_path = Path("output")
+        ))
+        _successfully_loaded_methods.append(grooming_method)
+    except FileNotFoundError:
+        logger.info(f"Skipping grooming method {grooming_method} because the output file isn't available")
+        
+# Convert to boost_histogram because it simplifies projections later.
+hists_data_central = {k: v.to_boost_histogram() for k, v in hists_data_central.items()}
+# -
+
+logger.info(f"\nSuccessfully loaded grooming methods: {_successfully_loaded_methods}")
+
+from importlib import reload
+reload(plot_from_skim)
+
+# ### Stats
+
+# +
+# Setup
+plot_png = False
+smeared_min_kt_central = 1.5
+
+_filenames = plot_from_skim.plot_Rg_vs_jet_pt_stats(
+    hists=hists_data_central,
+    grooming_methods=_all_available_methods,
+    prefix=prefix,
+    jet_pt_bin=jet_pt_bin,
+    rdf_plots=True,
+    output_dir=_output_dir,
+    plot_png=plot_png,
+    system_label=system_label,
+    smeared_min_kt=smeared_min_kt_central,
+)
+
+_filenames = plot_from_skim.plot_zg_vs_jet_pt_stats(
+    hists=hists_data_central,
+    grooming_methods=_all_available_methods,
+    prefix=prefix,
+    jet_pt_bin=jet_pt_bin,
+    rdf_plots=True,
+    output_dir=_output_dir,
+    plot_png=plot_png,
+    system_label=system_label,
+    smeared_min_kt=smeared_min_kt_central,
+)
+# -
+
+
+
+# ## Semi-central PbPb
+
+# ### Load data
+
+# +
+def grooming_name(grooming_method: str, prefixes: list[str]) -> str:
+    return f"{grooming_method}_prefixes_{'_'.join(prefixes)}"
+
+# Settings
+collision_system = "PbPb"
+prefix = "data"
+jet_pt_bin = helpers.JetPtRange(min=40, max=120)
+text_label = "Iterative splittings"
+text_label += "\n" + f"${jet_pt_bin.display_str(label='meas.')}$"
+tag = jet_pt_bin.histogram_str(prefix)
+system_label = plot_style.label_to_display_string["collision_system"][f"{collision_system}_5TeV"]
+
+# Setup
+#_all_available_methods = list(nb_utils.all_grooming_methods)
+_grooming_methods = [
+    "dynamical_core",
+    "dynamical_kt",
+    "dynamical_time",
+]
+_method_groups = [
+    _grooming_methods,
+    # Older
+    ##nb_utils.leading_kt_grooming_methods,
+    #nb_utils.dynamical_grooming_methods,
+    #nb_utils.soft_drop_grooming_methods,
+    #nb_utils.dynamical_grooming_with_z_cut,
+    #nb_utils.z_cut_02_grooming_methods,
+    ##_all_available_methods,
 ]
 # We want to skip the leading_kt methods, but not redefine all_grooming_methods in nb_utils,
 # so we combine all of the above
@@ -108,15 +319,14 @@ _output_dir.mkdir(parents=True, exist_ok=True)
 _fig_output_dir = _output_dir / "png"
 
 # Load data for comparison
-hists_data = {}
+hists_data_semi_central = {}
 _successfully_loaded_methods = []
 #dataset_name = "LHC18qr_semi_central_R02_6765"
-#dataset_name = "LHC18qr_semi_central_R02_0067"
-dataset_name = "LHC17pq_pp_R02_0060"
+dataset_name = "LHC18qr_semi_central_R02_0067"
 #for grooming_method in nb_utils.all_grooming_methods:
 for grooming_method in _grooming_methods:
     try:
-        hists_data.update(nb_utils.load_histograms(
+        hists_data_semi_central.update(nb_utils.load_histograms(
             filename = f"{dataset_name}_{grooming_name(grooming_method, [prefix])}.root", collision_system=collision_system,
             tag = "RDF", base_path = Path("output")
         ))
@@ -125,9 +335,44 @@ for grooming_method in _grooming_methods:
         logger.info(f"Skipping grooming method {grooming_method} because the output file isn't available")
         
 # Convert to boost_histogram because it simplifies projections later.
-hists_data = {k: v.to_boost_histogram() for k, v in hists_data.items()}
+hists_data_semi_central = {k: v.to_boost_histogram() for k, v in hists_data_semi_central.items()}
 # -
 
 logger.info(f"\nSuccessfully loaded grooming methods: {_successfully_loaded_methods}")
+
+from importlib import reload
+reload(plot_from_skim)
+
+# ### Stats
+
+# +
+# Setup
+plot_png = False
+smeared_min_kt_semi_central = 1.0
+
+_filenames = plot_from_skim.plot_Rg_vs_jet_pt_stats(
+    hists=hists_data_semi_central,
+    grooming_methods=_all_available_methods,
+    prefix=prefix,
+    jet_pt_bin=jet_pt_bin,
+    rdf_plots=True,
+    output_dir=_output_dir,
+    plot_png=plot_png,
+    system_label=system_label,
+    smeared_min_kt=smeared_min_kt_semi_central,
+)
+
+_filenames = plot_from_skim.plot_zg_vs_jet_pt_stats(
+    hists=hists_data_semi_central,
+    grooming_methods=_all_available_methods,
+    prefix=prefix,
+    jet_pt_bin=jet_pt_bin,
+    rdf_plots=True,
+    output_dir=_output_dir,
+    plot_png=plot_png,
+    system_label=system_label,
+    smeared_min_kt=smeared_min_kt_semi_central,
+)
+# -
 
 
